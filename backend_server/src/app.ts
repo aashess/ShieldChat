0905b2fa9c981ea9f20555b80da8442ec7169aac
aashish jha <aashish@aashishs-MacBook-Air.local>
2025-11-22 02:23:11 +0530

    websocket intalized

--- backend_server/src/app.ts
+++ backend_server/src/app.ts
@@ -2,28 +2,42 @@ import express from "express";
 import router from "./routes/user.js";
 import { createServer } from "node:http";
 import { Server } from "socket.io";
-
+import cors from 'cors';
 // import { fileURLToPath } from 'node:url';
 // import { dirname, join } from 'node:path';
 // import { Server } from 'socket.io';
 const app = express();
+app.use(cors());
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",   // Your React app URL (Vite example)
+    methods: ["GET", "POST"]
+  }
+});
 
-app.use("/", router);
+
+// app.use("/", router);
+
+
 
 io.on("connection", (socket) => {
-  console.log("A user connected");
-  socket.on("disconnect", () => {
-    console.log("A user disconnected");
-  });
+  console.log("User connected:", socket.id);
 
   socket.on("message", (data) => {
-    // sends the data to everyone connected to the server
-    socket.emit("response", data);
+    console.log("Received:", data);
+    io.emit("message", data); // send to all clients
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
   });
 });
 
-app.listen(3000, () => {
-  console.log("server running at http://localhost:3000");
+app.get("/", (req, res) => {
+  res.send("Hello from backend!");
+});
+
+server.listen(3000, () => {
+  console.log("Socket+Express running on 3000");
 });
